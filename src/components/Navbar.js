87193dd8ac933fd0e51d8a82935e901a8e683a41
ItsDavidHdez <david-0610@live.com.mx>
2021-06-13 02:52:14 -0500

    Redirect in new item navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -68,7 +68,7 @@ export default function Navbar(props) {
                     : "text-gray-800 hover:text-gray-600") +
                   " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                 }
-                href="#team"
+                href="#empresa"
               >
                 <span className="text-white text-sm">Quienes somos</span>
               </a>
