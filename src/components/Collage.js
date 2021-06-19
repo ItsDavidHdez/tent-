55fe0f15a12201d52df85f04f5faec49ac8bfcfe
ItsDavidHdez <david-0610@live.com.mx>
2021-06-19 13:18:57 -0500

    Stylesheet for Collage component ready

--- src/components/Collage.js
+++ src/components/Collage.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, Suspense, lazy } from "react";
 import { SliderData } from "./SliderData";
 import "./Collage.css";
+
+const Image = lazy(() => {
+  SliderData.map((pic) => pic.image);
+});
 
 const Collage = ({ slides, setShowCollage }) => {
   const [current, setCurrent] = useState(0);
@@ -18,23 +22,35 @@ const Collage = ({ slides, setShowCollage }) => {
     return null;
   }
   return (
-    <section className="slider">
+    <section className="fixed text-gray-500 flex items-center justify-center overflow-auto z-50 bg-gray-300 bg-opacity-10 left-0 right-0 top-0 bottom-0">
       <button className="left-arrow" onClick={prevSlide}>
-        left
+        <i className="fas fa-chevron-circle-left"></i>
       </button>
-      <button onClick={() => setShowCollage(false)}>X</button>
+      <button
+        className="close text-black text-4xl absolute right-16"
+        style={{ top: "15px", right: "25px" }}
+        onClick={() => setShowCollage(false)}
+      >
+        <i className="fas fa-times-circle text-black"></i>
+      </button>
       <button className="right-arrow" onClick={nextSlide}>
-        right
+        <i className="fas fa-chevron-circle-right"></i>
       </button>
       {SliderData.map((slide, index) => {
         return (
           <div
-            className={index === current ? "slide active" : "slide"}
+            className={index === current ? "slide active" : "relative"}
             key={index}
           >
-            {index === current && (
-              <img src={slide.image} alt="travelimage" className="image" />
-            )}
+            <Suspense fallback={<h1>Loading...</h1>}>
+              {index === current && (
+                <img
+                  src={slide.image}
+                  alt="travelimage"
+                  style={{ width: "450px" }}
+                />
+              )}
+            </Suspense>
           </div>
         );
       })}
--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,22 +1,26 @@
 export const SliderData = [
   {
     image:
-      "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      "https://bl6pap004files.storage.live.com/y4mcVIlOSSMYhgXZsAVdBReukkfFBNFGMhq5HH09_s2gimsX5jaQ4bYz2OkMPmmO3C3A9gaTEF_WUY16eRKVPuFk1PpILjPgLoK4laAiCvQ3s259ne3OMOlKa043j_2DzzetiwBgANFUODJJsazL5_Nvm2yOnwI3N4pnK85E5GtaPSWIMTkNx3QUez7EkkEu94N?width=433&height=579&cropmode=none",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80",
+      "https://bl6pap004files.storage.live.com/y4mzgDjA6AC-H_QmeZr2HGCcO82-zeyo5WIxVYSOTqLhfFpyCY1VCC61HCUcT1nkBl4FgqpZeyQfaZVFli_WLotdMgWT0kGQQFvGHj3JSjKPL3GcSleGDB_kGXr_NHiVO2CCTASE9ci-ZF2dUH1ziMpMZqOj2ebsbje8OYnzQQAhHPQiLWBTsrojUtzm_48MbUO?width=766&height=1024&cropmode=none",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+      "https://bl6pap004files.storage.live.com/y4m-zcLwdQdngty-cN-ryNn9IopFbq0uUNUnOZT5rD1m81YEEVgd_k2KjpSKks9t4r__ySoVJBeMpNUyKtB5jIZgfso144aa3qIp2SpCOipSFLP7CzMkyHTzyP78YtpjiawutdpxF3vZ7bWu3HWbwsKOG9bkILoyhGuTT9Q81BktRWz1Qs95bPOCbQBmTcvjRLa?width=933&height=1024&cropmode=none",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1475189778702-5ec9941484ae?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1351&q=80",
+      "https://bl6pap004files.storage.live.com/y4mzguBCAbPaljHKsFaxpCd7NeOb19LOv84c2KYJn-9vZfxSjO65QE2U_9DYwpWiSJ7YuZuDGO-klDQLYB5cTXfbaXKcvAJu9fwhldJCz2kLAEFeItSS8m3ouvIIlrL7XTnvLhjkZJLP8B97rLnqU1lBiKvGuQNJly0y6llclvfCJ8E2ul3hsMUEnAs6XXEywbl?width=960&height=1280&cropmode=none",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1503177119275-0aa32b3a9368?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+      "https://bl6pap004files.storage.live.com/y4mWJ0Xz0icP_iSEcXLyNRh77CcbrbS1puS34zpFZfVLzq_XoMCLnzgt-dHoC2LYm3GXLMaKQaDQfIxx8gl3KysxueYc78-s1ubpv_bqiFDfWLbPHl_pCZk-vHcugUjym3fwX7U9hVF6b44eHDcyKKvv4VXab0SmEXuSMFC_AsUBAJD0Shro51Og7bwJkVu1Ua9?width=960&height=1280&cropmode=none",
+  },
+  {
+    image:
+      "https://bl6pap004files.storage.live.com/y4mr44ahaTmj-SeUWbXQ1Gh4_fVXMOdls4ZDjG6xAfb0KSKJ66MOziVRRyPzttwfGOeV_HuaAms6v2VJRWVsl5h6-oMdwzSImtYWYDy2JX0_1yyrIbh4di_tCBKkqN78pCjjlQKUFoIzHj9FsCgeH4OrpvtW2-1n5hvhVM0YYwMJ5jMIwjT2N6ZPeBT9SjCMRn6?width=576&height=1024&cropmode=none",
   },
 ];
